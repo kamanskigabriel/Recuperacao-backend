f6b5aa198252f4eb144fbfa4f77c710af3c219dd
gabriel <gabrielkamanski0gmail.com>
2026-08-17 22:03:01 -0300

    PA-prova-terminada

--- router/contas.js
+++ router/contas.js
@@ -1,36 +1,33 @@
-
-import router from './router/contas'
 import banco from '../repository/banco'
+import express from 'express'
 
-const app = express()
-app.use(express())
+const router = express.Router()
 //--------------------------------------------------
-app.get("/somar", (req, res) => {
+router.get("/somar", (req, res) => {
     const { num1, num2} = req.body
     const resultado = Number(num1) + Number(num2)
 })
 //--------------------------------------------------
-app.post("/diminuir", (req, res) => {
+router.post("/diminuir", (req, res) => {
     const { num1, num2} = req.body
     const resultado = Number(num1) - Number(num2)
 })
 //--------------------------------------------------
-app.post("/subtrair", (req, res) => {
+router.post("/subtrair", (req, res) => {
     const { num1, num2} = req.body
     const resultado = Number(num1) / Number(num2)
 })
 //--------------------------------------------------
-app.post("/multiplicar", (req, res) => {
+router.post("/multiplicar", (req, res) => {
     const { num1, num2} = req.body
     const resultado = Number(num1) * Number(num2)
 })
 //--------------------------------------------------
-app.post("/potencia", (req, res) => {
+router.post("/potencia", (req, res) => {
     const { num1, num2} = req.body
     const resultado = Number(num1) * Number(num1)
+
+    
 })
-//--------------------------------------------------
-app.listen(3000, {} = (
-  console.log("Servidor esta na porta 3000")  
-))
+
 export default Router
